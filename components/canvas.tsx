35b50827c43d3b31cb741869f6e74d80fba160e0
katungi <dankatdennis@gmail.com>
2024-02-20 13:19:47 +0300

    convert html to a canvas then download it

--- components/canvas.tsx
+++ components/canvas.tsx
@@ -1,32 +1,28 @@
-import React from "react"
-import TicketDemo from "./ticket"
-
+import React from "react";
+import TicketDemo from "./ticket";
+import html2canvas from 'html2canvas';
 
 export default function TicketCanvas() {
+    const ticketRef = React.useRef(null);
 
-    const canvasRef = React.useRef(null)
-
-    React.useEffect(() => {
-        const canvas = canvasRef.current
+    const downloadImage = () => {
+        const ticketElement = ticketRef.current;
         // @ts-ignore
-        const context = canvas?.getContext('2d')
-        context.fillRect(0, 0, context.canvas.width, context.canvas.height)
-    }, [])
+        html2canvas(ticketElement).then((canvas) => {
+            const image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+            const a = document.createElement('a');
+            a.href = image;
+            a.download = 'ticket.png';
+            a.click();
+        });
+    };
 
     return (
-       <>
-        <canvas ref={canvasRef} className="">
-            <TicketDemo />
-        </canvas>
-        <button onClick={() => {
-            const canvas = canvasRef.current
-            // @ts-ignore
-            const image = canvas?.toDataURL("image/png").replace("image/png", "image/octet-stream")
-            const a = document.createElement('a')
-            a.href = image
-            a.download = 'ticket.png'
-            a.click()
-        } }>Download</button>
-       </>
-    )
+        <>
+            <div ref={ticketRef}>
+                <TicketDemo />
+            </div>
+            <button onClick={downloadImage}>Download</button>
+        </>
+    );
 }
